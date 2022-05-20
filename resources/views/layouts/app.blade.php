78cb434864797260a7247ec9f23c7c8ca82a6989
Dajamplay <ggyoug@gmail.com>
2022-05-20 23:06:58 +0300

    Added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,8 @@
 
 <header id="header" class="fixed-top">
     <div class="container-fluid d-flex justify-content-between align-items-center">
-        <span class="logo me-auto me-lg-0"><a href="/">Елеанта</a></span>
-{{--        <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>--}}
+{{--        <span class="logo me-auto me-lg-0"><a href="#" class="logo"><img src="/img/logo.png" alt="" class="img-fluid"></a></span>--}}
+        <a href="#" class="logo hue"><img src="/img/logo.png" alt="Елеанта"></a>
         <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
                 <li><a class="active" href="/">Главная</a></li>
